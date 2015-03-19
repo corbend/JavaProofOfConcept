e3ff965a0e5830d39eceb2a26e6b0c5b101952d7
alexn <thinkandwin5000@gmail.com>
2015-03-19 22:48:52 +0300

    test of Joda time lib in conjuction with Redis

--- src/main/java/org/monolith/exp/App.java
+++ src/main/java/org/monolith/exp/App.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 import java.util.logging.Logger;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 import org.monolith.exp.WritableObject;
 import redis.clients.jedis.Jedis;
 
@@ -18,6 +19,26 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class App {
+
+    private static Jedis client = new Jedis("localhost", 6379);
+
+    public static void playWithTime() {
+
+        DateTime date = DateTime.now().withMillis(0);
+        String dateToRedis = date.toString(DateTimeFormat.forPattern("YYYY-MM-dd'T'hh:mm:ss"));
+
+        client.lpush("DateTimeKey", dateToRedis);
+
+        String value = client.lpop("DateTimeKey");
+
+        DateTime readDate = DateTime.parse(value).withMillis(0);
+
+        if (readDate.equals(date)) {
+            System.out.println("Dates is equal!");
+        } else {
+            System.out.println("Date read error!" + readDate + "<>" + date);
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -34,7 +55,7 @@ public class App {
             o = em.find(WritableObject.class, 1L);
             o.toString();
         } catch (NullPointerException e) {
-            
+
             o = new WritableObject();
             o.setId(1L);
             o.setName("ABC");
@@ -51,8 +72,7 @@ public class App {
         }
 
         String uid = "1";
-        String jsonData = JsonWriter.objectToJson(o);
-        Jedis client = new Jedis("localhost", 6379);
+
         Object uncastObject = client.get("TEST#" + uid);
 
         if (uncastObject == null) {
@@ -64,6 +84,7 @@ public class App {
             System.out.println("PRE WRITE FROM REDIS->" + "(" +  uid + ")" + "=" + preWriteObject.toString());
         }
 
+        String jsonData = JsonWriter.objectToJson(o);
         client.set("TEST#" + uid, jsonData);
 
         String stringObject = client.get("TEST#" + uid);
@@ -77,5 +98,6 @@ public class App {
 
         System.out.println("POST WRITE FROM REDIS->" + "(" +  uid + ")" + "=" + redisObject.toString());
 
+        playWithTime();
     }
 }
